309ce60c3c3d9c4e03a50507452f7744b44c596b
Oleksandr Andrushchenko <andr2000@gmail.com>
2016-11-11 09:35:15 +0200

    Remove "based on"
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- xen-drm.c
+++ xen-drm.c
@@ -1,8 +1,6 @@
 /*
  *  Xen para-virtual DRM device
  *
- *  Based on drivers/gpu/drm/rcar-du
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
